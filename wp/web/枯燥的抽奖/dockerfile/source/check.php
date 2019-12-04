ee87e021f32867f4cb5de83b77cdc9a2c53b6e1e
Xi4or0uji <38835587+Xi4or0uji@users.noreply.github.com>
2019-12-04 19:48:00 +0800

    Add files via upload

---
+++ wp/web/枯燥的抽奖/dockerfile/source/check.php
@@ -0,0 +1,28 @@
+<?php
+
+header("Content-Type: text/html;charset=utf-8");
+session_start();
+if(!isset($_SESSION['seed'])){
+$_SESSION['seed']=rand(0,999999999);
+}
+mt_srand($_SESSION['seed']);
+$str_long1 = "abcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+$str='';
+$len1=20;
+for ( $i = 0; $i < $len1; $i++ ){
+    $str.=substr($str_long1, mt_rand(0, strlen($str_long1) - 1), 1);       
+}
+$str_show = substr($str, 0, 10);
+echo "<p id='p1'>".$str_show."</p>";
+
+
+if(isset($_POST['num'])){
+	if($_POST['num']===$str){
+		echo "<p id=flag>抽奖，就是那么枯燥且无味，给你flag{Y0u_wIN_abcdefghijklmnopqrstuvwxyz}</p>";
+	}
+	else{
+		echo "<p id=flag>没抽中哦，再试试吧</p>";
+	}
+}
+
+show_source("taozijunwelcomeyou.php");
